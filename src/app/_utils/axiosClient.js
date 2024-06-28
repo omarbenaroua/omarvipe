92e8b104088273f01c35c32f1ea5cf52a0d616c0
omarbenaroua <74268199+omarbenaroua@users.noreply.github.com>
2024-06-28 02:40:51 +0200

    Update axiosClient.js

--- src/app/_utils/axiosClient.js
+++ src/app/_utils/axiosClient.js
@@ -1,7 +1,7 @@
 const { default: axios } = require("axios");
 
 const apiKey=process.env.NEXT_PUBLIC_REST_API_KEY;
-const apiUrl='https://omarvipe-strapi-1.onrender.com/api';
+const apiUrl='https://omarvipe-strapi.onrender.com/api';
 const axiosClient= axios.create({
     baseURL : apiUrl,
     headers : {
